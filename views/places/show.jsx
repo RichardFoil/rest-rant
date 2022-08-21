3fad64429e4610dd0791dbfd95b07d241a867e11
Richard Foil <103152246+RichardFoil@users.noreply.github.com>
2022-08-21 10:35:12 -0400

    trying to get data to show up on place

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,9 +6,14 @@ function show (data) {
         <Def>
           <main>
             <h1>{ data.place.name }</h1>
-            <h2> Rating </h2>
-            <h2>Description</h2>
-            <h2> Comments</h2>
+            <h2>{ data.place.pic }</h2>
+            <h3> Rating </h3>
+                <p>
+                    currently unrated
+                </p>
+            <h3>Description</h3>
+            Located in { data.place.city },{ data.place.state } and serving { data.place.cuisines }
+            <h3> Comments</h3>
           </main>
         </Def>
     )
