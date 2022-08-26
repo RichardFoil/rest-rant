3c0c85b90112dd22a02372118ad47372f849057d
Richard Foil <103152246+RichardFoil@users.noreply.github.com>
2022-08-25 20:54:30 -0400

    mongoosed the restrant

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -20,13 +20,18 @@ function New () {
                     <input className="form-control" id="city" name="city" />
                 </div>
                 <div className="form-group">
-                 <label htmlFor="state">State</label>
-                 <input className="form-control" id="state" name="state" />
+                    <label htmlFor="state">State</label>
+                    <input className="form-control" id="state" name="state" />
                  </div>
                 <div className="form-group">
-                 <label htmlFor="cuisines">Cuisines</label>
-                 <input className="form-control" id="cuisines" name="cuisines" required />
+                    <label htmlFor="cuisines">Cuisines</label>
+                    <input className="form-control" id="cuisines" name="cuisines" required />
                 </div>
+                <div className="form-group">
+                    <label for="founded">Founded Year</label>
+                    <input className="form-control" id="founded" name="founded" />
+                </div>
+
                 <input className="btn btn-primary" type="submit" value="Add Place" />
             </form>
           </main>
