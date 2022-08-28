061af4f86a116b79eee3015243ce66bfeb1330c1
Richard Foil <103152246+RichardFoil@users.noreply.github.com>
2022-08-28 11:10:51 -0400

    updated schema

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,13 +8,23 @@ function show (data) {
           <main>
             <h1>{ data.place.name }</h1>
             <img src={data.place.pic} alt={data.place.name}></img>
+                <h3>
+                Located in { data.place.city },{ data.place.state }
+                </h3>
                 <h3> Rating </h3>
                 <p>
                     currently unrated
                 </p>
-            <h3>Description</h3>
-            Located in { data.place.city },{ data.place.state } and serving { data.place.cuisines }
-            <h3> Comments</h3>
+            <h2>Description</h2>
+            <h3>
+                {data.place.showEstablished()}
+            </h3>
+            <h4>
+                Serving {data.place.cuisines}
+            </h4>
+
+           
+
 
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                 Edit
@@ -24,8 +34,10 @@ function show (data) {
                 <button type="submit" className="btn btn-danger">
                     Delete
                 </button>
+           
             </form>     
- 
+            
+            <h3> Comments</h3>
 
           </main>
         </Def>
