ae6abdf366803b8fd57c2115d437367e25154447
Richard Foil <103152246+RichardFoil@users.noreply.github.com>
2022-09-01 11:39:30 -0400

    Populate the Comments

--- controllers/places.js
+++ controllers/places.js
@@ -30,7 +30,9 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+  .populate('comments')
   .then(place => {
+      console.log(place.comments)
       res.render('places/show', { place })
   })
   .catch(err => {
@@ -38,6 +40,7 @@ router.get('/:id', (req, res) => {
       res.render('error404')
   })
 })
+
 
 
 router.put('/:id', (req, res) => {
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,43 +2,61 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
-     console.log(data)
+     let comments = (
+        <h3 className="inactive">
+            No Comments Yet!
+        </h3>
+     )
+     if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+          return (
+            <div className="border">
+              <h2 className="rant">{c.rant ? 'Rant!' : 'Rave! '}</h2>
+              <h4>{c.content}</h4>
+              <h3>
+                <stong>- {c.author}</stong>
+              </h3>
+              <h4>Rating: {c.stars}</h4>
+            </div>
+          )
+        })
+      }
     return (
         <Def>
           <main>
-            <h1>{ data.place.name }</h1>
-            <img src={data.place.pic} alt={data.place.name}></img>
+            <div className="row">
+                <h1>{ data.place.name }</h1>
+                <img src={data.place.pic} alt={data.place.name}></img>
+                    <h3>
+                    Located in { data.place.city },{ data.place.state }
+                    </h3>
+                    <h3> Rating </h3>
+                    <p>
+                        currently unrated
+                    </p>
+                <h2>Description</h2>
                 <h3>
-                Located in { data.place.city },{ data.place.state }
+                    {data.place.showEstablished()}
                 </h3>
-                <h3> Rating </h3>
-                <p>
-                    currently unrated
-                </p>
-            <h2>Description</h2>
-            <h3>
-                {data.place.showEstablished()}
-            </h3>
-            <h4>
-                Serving {data.place.cuisines}
-            </h4>
+                <h4>
+                    Serving {data.place.cuisines}
+                </h4>
 
-           
-
-
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-                Edit
-            </a>  
-           
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-                <button type="submit" className="btn btn-danger">
-                    Delete
-                </button>
-           
-            </form>     
             
-            <h3> Comments</h3>
-
+                <a id="editbutton" href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                    Edit
+                </a>  
+            
+                <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                    <button type="submit" className="btn btn-danger">
+                        Delete
+                    </button>
+            
+                </form> 
+            </div>    
+            <hr />
+            <h2> Comments</h2>
+            {comments}
           </main>
         </Def>
     )
